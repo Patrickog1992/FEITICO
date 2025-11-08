187532b239467f6eda1139014dfd51560a022172
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-08 14:10:08 +0000

    o video ainda está começando sozinho

--- src/components/feiticos/landing-page-nova.tsx
+++ src/components/feiticos/landing-page-nova.tsx
@@ -61,7 +61,29 @@ const VturbVideoPlayer = () => {
 };
 
 const VideoPlayer = () => {
-  return <VturbVideoPlayer />;
+    const [showVideo, setShowVideo] = useState(false);
+
+    if (showVideo) {
+        return <VturbVideoPlayer />;
+    }
+
+    return (
+        <div className="relative cursor-pointer" onClick={() => setShowVideo(true)}>
+            <Image
+                src="https://i.imgur.com/2hnUgvP.png"
+                alt="Play Video"
+                width={400}
+                height={225}
+                className="mx-auto"
+                style={{ maxWidth: '400px', width: '100%' }}
+            />
+             <div className="absolute inset-0 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="rgba(255,255,255,0.7)">
+                    <path d="M8 5v14l11-7z" />
+                </svg>
+            </div>
+        </div>
+    );
 };
 
 

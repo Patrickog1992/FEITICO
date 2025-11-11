a465584f9dbd52d7b261d0d1e40e78c6842e733e
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-11 13:29:21 +0000

    na página lp
    quando a pessoa clicar em algum botão clicavel e ir para a

--- src/components/feiticos/step1-modal-lp.tsx
+++ src/components/feiticos/step1-modal-lp.tsx
@@ -81,7 +81,7 @@ export default function Step1Modal({ onComplete }: Step1ModalProps) {
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
-    window.location.href = 'https://pay.kirvano.com/9e2a7067-9ff4-4612-9f68-0b355238ae45';
+    window.location.href = 'https://pay.kirvano.com/5fbfabed-b287-43fb-9640-735dfb2a99f3';
   }
 
   return (

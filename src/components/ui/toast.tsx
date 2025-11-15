ac26e7a331ca38ca8a7181522621c484d3e51d5a
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-15 10:29:36 +0000

    menor

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -16,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 right-0 z-[100] flex max-h-screen flex-col-reverse p-4 sm:flex-col md:max-w-[280px]",
+      "fixed top-0 right-0 z-[100] flex max-h-screen w-full max-w-[240px] flex-col-reverse p-4",
       className
     )}
     {...props}

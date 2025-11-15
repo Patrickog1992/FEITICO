44b80cd333982f698a82cd0f1f2c1a4e2290b932
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-15 10:29:04 +0000

    ele ainda está no tamanho total

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 right-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:top-0 sm:right-0 sm:flex-col md:max-w-[280px]",
+      "fixed top-0 right-0 z-[100] flex max-h-screen flex-col-reverse p-4 sm:flex-col md:max-w-[280px]",
       className
     )}
     {...props}

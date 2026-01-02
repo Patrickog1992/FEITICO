4ef8627316a05111a03c130ea9616082f0430f2a
Patrick Oliveira <patrickpneus2018@hotmail.com>
2026-01-02 22:58:06 +0000

    ATENÇÃO: No dia 2 de janeiro, as energias cósmicas estão mais abertas de

--- src/components/feiticos/dynamic-date-banner.tsx
+++ src/components/feiticos/dynamic-date-banner.tsx
@@ -23,7 +23,7 @@ export default function DynamicDateBanner() {
 
   return (
     <div className="bg-yellow-400 text-black text-center p-2 text-sm font-semibold w-full max-w-4xl mx-auto rounded-lg mb-4">
-      ATENÇÃO: No dia <span className="bg-white text-yellow-500 px-2 py-1 rounded-md mx-1">{currentDate}</span>, as energias cósmicas estão mais abertas devido ao final de ano e o feitiço de Lady Soraya será até 3x mais forte, aproveite!
+      ATENÇÃO: No dia <span className="bg-white text-yellow-500 px-2 py-1 rounded-md mx-1">{currentDate}</span>, as energias cósmicas estão mais abertas devido a nesse começo de ano e o feitiço de Lady Soraya será até 3x mais forte, aproveite!
     </div>
   );
 }

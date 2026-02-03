38132d5825dc42d14ee7ee832872be6635d20416
Patrick Oliveira <patrickpneus2018@hotmail.com>
2026-02-03 13:37:15 +0000

    troque o video por esse
    <script type="text/javascript"> var s=document.

--- src/app/upsell2/page.tsx
+++ src/app/upsell2/page.tsx
@@ -16,14 +16,21 @@ export default function Upsell2Page() {
         </p>
       </header>
       <main>
-        <div 
-          dangerouslySetInnerHTML={{ __html: `<vturb-smartplayer id="vid-6981f7cb8cb5dc32dbfcb8c9" style="display: block; margin: 0 auto; width: 100%; max-width: 400px;"></vturb-smartplayer>` }}
-        />
-        <Script 
-          src="https://scripts.converteai.net/66f7b845-c900-4cf3-b6f2-f848ebd65e7f/players/6981f7cb8cb5dc32dbfcb8c9/v4/player.js" 
-          strategy="afterInteractive"
-          async
-        />
+        <div dangerouslySetInnerHTML={{ __html: `
+          <div id="ifr_6981f7cb8cb5dc32dbfcb8c9_wrapper" style="margin: 0 auto; width: 100%; max-width: 400px;">
+            <div style="position: relative; padding: 177.77777777777777% 0 0 0;" id="ifr_6981f7cb8cb5dc32dbfcb8c9_aspect">
+              <iframe frameborder="0" allowfullscreen src="about:blank" id="ifr_6981f7cb8cb5dc32dbfcb8c9" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" referrerpolicy="origin" onload="this.onload=null, this.src='https://scripts.converteai.net/66f7b845-c900-4cf3-b6f2-f848ebd65e7f/players/6981f7cb8cb5dc32dbfcb8c9/v4/embed.html' +(location.search||'?') +'&vl=' +encodeURIComponent(location.href)"></iframe>
+            </div>
+          </div>
+        `}} />
+        <Script id="v-turb-script-loader" strategy="afterInteractive">
+          {`
+            var s=document.createElement("script"); 
+            s.src="https://scripts.converteai.net/lib/js/smartplayer-wc/v4/sdk.js"; 
+            s.async=true;
+            document.head.appendChild(s);
+          `}
+        </Script>
       </main>
     </div>
   );
